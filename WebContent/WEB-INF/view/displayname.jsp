a24ae34b21cf2f650763d3856a909648ad717967
brajesh <brajesh.kumar@squareyards.co.in>
2019-09-25 23:12:25 +0530

    add new way to access request param

--- WebContent/WEB-INF/view/displayname.jsp
+++ WebContent/WEB-INF/view/displayname.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
- Hello <%= request.getAttribute("firstname") %>
+ Hello ${firstname}
 </body>
 </html>
--- src/org/studyself/MainController.java
+++ src/org/studyself/MainController.java
@@ -4,8 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -14,9 +16,14 @@ public class MainController {
 		return "form";
 	}
 	@RequestMapping(value = "displayname")
-	public String displayNmae(HttpServletRequest request,HttpServletResponse response){
-		String firstname=request.getParameter("firstname");
-		request.setAttribute("firstname",firstname);
+//	public String displayNmae(HttpServletRequest request,HttpServletResponse response){
+//		String firstname=request.getParameter("firstname");
+//		request.setAttribute("firstname",firstname);
+//		return "displayname";
+//	}
+	public String displayNmae(@RequestParam("firstname") String name ,Model model){
+		
+		model.addAttribute("firstname",name);
 		return "displayname";
 	}
 }
